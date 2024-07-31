e85f7bdf5d59c95a46d4e3667bde2e6f942ff10f
Mike Sutton <mikesutton@gmail.com>
2024-07-30 21:10:52 -0600

    work around on echo; maybe working?

---
+++ README.md
@@ -0,0 +1,7 @@
+
+# References
+
+* DS: https://files.waveshare.com/upload/e/e1/DS_SX1261-2_V1.2.pdf
+* Another sx1262 project: https://github.com/chandrawi/LoRaRF-Python
+* Waveshare page: https://www.waveshare.com/pico-lora-sx1262-868m.htm
+* LoRa packet xmision time calculator: https://www.semtech.com/design-support/lora-calculator
--- my_sx1262.py
+++ my_sx1262.py
@@ -37,6 +37,19 @@ pin_dio1 = Pin(20, Pin.IN)
 pin_led = Pin('LED', Pin.OUT)
 
 
+class IRQ_BITS:
+    TXDONE = 1 << 0
+    RXDONE = 1 << 1
+    PREAMBLE_DETECTED = 1 << 2
+    SYNC_WORD_VALID = 1 << 3
+    HEADER_VALID = 1 << 4
+    HEADER_ERR = 1 << 5
+    CRC_ERR = 1 << 6
+    CAD_DONE = 1 << 7
+    CAD_DETECTED = 1 << 8
+    TIMEOUT = 1 << 9
+
+
 spi = SPI(1, sck=pin_sck, mosi=pin_mosi, miso=pin_miso)
 
 def op_code(code, total_bytes):
@@ -49,6 +62,17 @@ def op_code(code, total_bytes):
     
     pin_cs.value(1)
     return buff_in
+
+
+def wait_not_busy():
+    while pin_busy.value() == 1:
+        time.sleep(.1)
+
+
+def reset():
+    pin_reset.value(0)
+    time.sleep(.1)
+    pin_reset.value(1)
 
 
 status_modes = {
@@ -85,6 +109,7 @@ def get_status():
     0x5: failure to execute command
     0x6: command tx done (xmision terminated)
     """
+    wait_not_busy()
     buff = bytearray(2)
     buff[0] = 0xc0  # getStatus
     buff_in = bytearray(2)
@@ -100,6 +125,7 @@ def get_status():
 
 
 def set_lora():
+    wait_not_busy()
     buff = bytearray(2)
     buff[0] = 0x8a
     buff[1] = 0x01
@@ -109,6 +135,7 @@ def set_lora():
 
 
 def write_buffer(offset, data):
+    wait_not_busy()
     buff = bytearray(2 + len(data))
     buff[0] = 0x0e
     buff[1] = offset
@@ -121,6 +148,7 @@ def write_buffer(offset, data):
 def read_buffer(offset, num):
     """
     First byte is status, then data"""
+    wait_not_busy()
     buff = bytearray(3 + num)
     buff_in = bytearray(3 + num)
     buff[0] = 0x1e
@@ -134,10 +162,11 @@ def set_buffer_base_addr():
     """
     pg. 93
     """
+    wait_not_busy()
     buff = bytearray(3)
     buff[0] = 0x8f
-    buff[0] = 0  # tx 
-    buff[0] = 0  # rx
+    buff[1] = 0  # tx 
+    buff[2] = 128  # rx
     pin_cs.value(0)
     spi.write(buff)
     pin_cs.value(1)
@@ -148,6 +177,7 @@ def set_pa_config():
     pg. 76
     Think I have the SX1262
     """
+    wait_not_busy()
     buff = bytearray(5)
     buff[0] = 0x95
     # max power?  +22 dBm
@@ -164,6 +194,7 @@ def set_tx_params():
     """
     pg. 84
     """
+    wait_not_busy()
     buff = bytearray(3)
     buff[0] = 0x8e
     buff[1] = 0x16  # 22 dBm
@@ -188,6 +219,7 @@ def set_packet_params(payload_size):
     
     pg 87 describes parameters (modulation parameters? not packet?)
     """
+    wait_not_busy()
     buff = bytearray(10)
     buff[0] = 0x8c
     # buff[1] = 
@@ -206,13 +238,15 @@ def set_modulation_params():
     """
     pg. 87
     """
+    wait_not_busy()
     buff = bytearray(9)
     buff[0] = 0x8b
     # buff[1] = 0x1 # SF5  pg 87
     buff[1] = 0x0c # SF7  pg 87 longest time on error
     # buff[2] = 0x0  # BW 7.81 kHz  (lowest)
+    buff[2] = 0x4  # 125 kHz
     buff[3] = 0x04  # CR 1-4 higher has more error immunity
-    # buff[4] = 0 # low data rate optimize if  0x01
+    buff[4] = 1 # low data rate optimize if  0x01
     pin_cs.value(0)
     spi.write(buff)
     pin_cs.value(1)
@@ -223,6 +257,7 @@ def set_tx(timeout=0):
     timeout = 15.625 us * timeout
     pg 68
     """
+    wait_not_busy()
     buff = bytearray(4)
     buff[0] = 0x83
     # Timeout not implemented
@@ -237,6 +272,7 @@ def set_standby(mode):
     1: STBY_XOSC
     pg. 68
     """
+    wait_not_busy()
     buff = bytearray(2)
     buff[0] = 0x80
     buff[1] = mode
@@ -250,6 +286,7 @@ def get_errors():
     pg. 98
     Status codes on page: 95
     """
+    wait_not_busy()
     buff = bytearray(4)
     buff[0] = 0x17
     buff_in = bytearray(4)
@@ -264,6 +301,7 @@ def clear_errors():
     """
     pg. 98
     """
+    wait_not_busy()
     buff = bytearray(3)
     buff[0] = 0x07
     pin_cs.value(0)
@@ -278,6 +316,7 @@ def set_reg_mode(mode):
     0: LDO only
     1: DC + DC and LDO
     """
+    wait_not_busy()
     buff = bytearray(2)
     buff[0] = 0x96
     buff[1] = mode
@@ -290,6 +329,7 @@ def set_rf_freq(freq):
     """
     try 902300000
     """
+    wait_not_busy()
     rfFreq = int(freq * 33554432 / 32000000)
     # buf = (
     #         (rfFreq >> 24) & 0xFF,
@@ -309,16 +349,18 @@ def set_rf_freq(freq):
     pin_cs.value(1)
 
 
-def set_rx(timeout=0):
+def set_rx(timeout_sec=0):
     """
     pg. 69
     0xFFFFFF for continuous mode
     """
+    wait_not_busy()
+    timeout_int = int(timeout_sec / 15.625e-6)
     buf = bytearray(4)
     buf[0] = 0x82
-    buf[1] = (timeout >> 16) & 0xff
-    buf[2] = (timeout >> 8) & 0xff
-    buf[3] = timeout & 0xff
+    buf[1] = (timeout_int >> 16) & 0xff
+    buf[2] = (timeout_int >> 8) & 0xff
+    buf[3] = timeout_int & 0xff
     pin_cs.value(0)
     spi.write(buf)
     pin_cs.value(1)
@@ -328,6 +370,7 @@ def get_irq_status():
     """
     pg. 80
     """
+    wait_not_busy()
     buf = bytearray(4)
     buf_in = bytearray(4)
     buf[0] = 0x12
@@ -342,6 +385,7 @@ def clear_irq_status(mask):
     """
     pg. 81
     """
+    wait_not_busy()
     buf = bytearray(3)
     buf[0] = 0x02
     buf[1] = (mask >> 8) & 0xff
@@ -361,6 +405,7 @@ def set_dio_irq_params(mask):
     """
     pg 79
     """
+    wait_not_busy()
     buf = bytearray(9)
     buf[0] = 0x08
     buf[1] = (mask >> 8) & 0xff
@@ -375,6 +420,7 @@ def set_dio_irq_params(mask):
 
 
 def SetDIO2AsRfSwitchCtrl():
+    wait_not_busy()
     buf = bytearray(2)
     buf[0] = 0x9d
     buf[1] = 0x1  # DIO2 as RF switch
@@ -387,6 +433,7 @@ def SetDIO3AsTCXOCtrl(delay=5 << 6):
     """
     pg 81
     """
+    wait_not_busy()
     buf = bytearray(5)
     buf[0] = 0x97
     # buf[1] = 0x07  # 3.3 V? not sur what this should be
@@ -404,6 +451,7 @@ def GetRxBufferStatus():
     pg 96
     returns 3 bytes: Status, PayloadLengthRx, RxStartBufferPointer
     """
+    wait_not_busy()
     buf = bytearray(4)
     buf[0] = 0x13
     buf_in = bytearray(4)
@@ -416,17 +464,17 @@ def GetRxBufferStatus():
 def tx(payload: bytes):
     # Basic transmit description page 99
     # 2. set_lora
-    SetDIO2AsRfSwitchCtrl()
-    SetDIO3AsTCXOCtrl()
-    set_lora()
-    # 3. set_rf_freq
-    set_rf_freq(902300000)
+    # SetDIO2AsRfSwitchCtrl()
+    # SetDIO3AsTCXOCtrl()
+    # set_lora()
+    # # 3. set_rf_freq
+    # set_rf_freq(902300000)
 
-    # 4. PA config
-    set_pa_config()
+    # # 4. PA config
+    # set_pa_config()
 
-    # 5. set_tx_power
-    set_tx_params()
+    # # 5. set_tx_power
+    # set_tx_params()  #***
 
     # 6. set_buffer_base_addr
     set_buffer_base_addr()
@@ -441,21 +489,37 @@ def tx(payload: bytes):
     set_packet_params(len(payload))
 
     # 10. config DIO and IRQ:  TxDone IRQ and map this IRQ to a DIO (DIO1, DIO2 or DIO3)
-    set_dio_irq_params(0x1)
+    # set_dio_irq_params(0x1)
+    set_dio_irq_params(0x203)
     # 11. sync word
     # 12. transmit
     set_tx()
 
 
-def rx():
-    # 1. set stby
-    # 2. set lora
-    clear_errors()
+def init():
+    reset()
+    time.sleep(.5)
+
     SetDIO2AsRfSwitchCtrl()
     SetDIO3AsTCXOCtrl()
     set_lora()
     # 3. set frequency
     set_rf_freq(902300000)
+    set_pa_config()
+    set_tx_params()
+
+    clear_errors()
+
+
+def rx(timeout=30):
+    # 1. set stby
+    # 2. set lora
+    #clear_errors()
+    # SetDIO2AsRfSwitchCtrl()
+    # SetDIO3AsTCXOCtrl()
+    # set_lora()
+    # # 3. set frequency
+    # set_rf_freq(902300000)
     # 4. set base addr
     set_buffer_base_addr()
     # 5. modulation params
@@ -463,11 +527,12 @@ def rx():
     # 6. frame format
     set_packet_params(50)  # max rx length
     # 7. IRQs: to select the IRQ RxDone and map this IRQ to a DIO (DIO1 or DIO2 or DIO3), set IRQ Timeout as well
-    set_dio_irq_params(0x202)
+    # set_dio_irq_params(0x203)
+    set_dio_irq_params(IRQ_BITS.RXDONE | IRQ_BITS.TIMEOUT | IRQ_BITS.HEADER_ERR)
 
     # 8. sync word
     # 9. setrx
-    set_rx()
+    set_rx(timeout)
 
 
 def led_blink(num, delay=0.25):
@@ -483,6 +548,8 @@ def status_blink():
     led_blink(mode, .25)
 
 
+tim = Timer(-1)
+
 def main():
     
     # await asyncio.sleep_ms(2000)
@@ -493,7 +560,6 @@ def main():
     # led_blink(5)
     status_blink()
     
-    tim = Timer(-1)
     tim.init(mode=Timer.PERIODIC, period=5000, callback=lambda x:status_blink())
     
     # tx(b'strobe')
@@ -504,29 +570,57 @@ payload = b'empty'
 
 def dio_echo_irq(pin):
     
-
     tmp = get_irq_status()
-    print(tmp)
+    print('irq status:', tmp)
+    clear_irq_status(0x203)
 
     if tmp[2] & 0x2 == 0x2:
         # rxdone
         led_blink(1)
+
+        tim.deinit()
         
         buf = GetRxBufferStatus() # Status, PayloadLengthRx, RxStartBufferPointer
         payload = read_buffer(buf[2], buf[1])
+        print('echo rcv:', payload)
         set_standby(0)
         tx(payload)
+        # rx()
 
     elif tmp[2] & 0x1 == 0x1:
         # txdone
-        led_blink(2)
+        # led_blink(2)
     
+        print("echo irq xmit complete")
         # tx()
         # back to rx
-        set_standby(0)
+        # set_standby(0)
+        # time.sleep(.5)
+        # _, mode, cmd, _, _ = get_status()
+        # while mode != 2:
+        #     time.sleep(.5)
+        #     _, mode, cmd, _, _ = get_status()
+
+        # while pin_busy.value() == 1:
+        #     time.sleep(.5)
+
+        # time.sleep(5)
+        print("entering rx mode")
+        rx(0.5)
+    elif tmp[1] & 0x2 == 0x2:
+        # timeout
+        print("timeout")
         rx()
+
+        # set_packet_params(50)  # max rx length
+        # # 7. IRQs: to select the IRQ RxDone and map this IRQ to a DIO (DIO1 or DIO2 or DIO3), set IRQ Timeout as well
+        # set_dio_irq_params(0x202)
+
+        # # 8. sync word
+        # # 9. setrx
+        # set_rx()
     
-    clear_irq_status(0x3)
+    
 
 
 def dio_rx_irq(pin):
@@ -540,13 +634,14 @@ def dio_rx_irq(pin):
         
         buf = GetRxBufferStatus() # Status, PayloadLengthRx, RxStartBufferPointer
         payload = read_buffer(buf[2], buf[1])
-        print("rcvd", payload)
+        print("rx irq rcvd", payload)
         set_standby(0)
         # tx(payload)
         # pass
     elif tmp[2] & 0x1 == 0x1:
         # txdone
         led_blink(2) 
+        print("rx irq xmit complete")
     
         # tx()
         # back to rx
@@ -575,15 +670,16 @@ def dio_strobe_irq(pin):
     elif tmp[2] & 0x1 == 0x1:
         # txdone
         led_blink(2)
-
+        time.sleep(2)
+        
         tx(b'strobe')
         # back to rx
         
     
     clear_irq_status(0x3)
 
-# pin_dio1.irq(dio_echo_irq, trigger=Pin.IRQ_RISING)
-pin_dio1.irq(dio_rx_irq, trigger=Pin.IRQ_RISING)
+pin_dio1.irq(dio_echo_irq, trigger=Pin.IRQ_RISING)
+# pin_dio1.irq(dio_rx_irq, trigger=Pin.IRQ_RISING)
 
 
 # Lora model description pg. 37
